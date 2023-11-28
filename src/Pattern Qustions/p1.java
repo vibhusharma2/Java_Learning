44ff18c46c4a4779d7d10574a31e8d07c6350fcd
Vibhu Sharma <vibhu14sharma@gmail.com>
2023-11-28 21:45:52 +0530

    input taking, pattern qustion by nested loops

---
+++ src/Pattern Qustions/p1.java
@@ -0,0 +1,26 @@
+// Using nested loops=> using multiple loops
+
+/* *****
+   *****
+   *****
+   ***** */
+// print this pattern.
+
+import java.util.*;
+
+public class p1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        // outer loop
+        for(int i=1;i<=n;i++){
+            // inner loop
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+
+    }
+}
---
+++ src/Pattern Qustions/p2.java
@@ -0,0 +1,32 @@
+// print the pattern
+/*
+  *****
+  *   *
+  *   *
+  *****
+*/
+import java.util.*;
+public class p2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // outer loop
+        for(int i=1; i<=n; i++){
+            //inner loop
+            for(int j=1; j<=m; j++) {
+
+                // cell=> (i,j)
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.println(" ");
+                }
+            }
+        }
+            System.out.println();
+
+    }
+}
