8ea5525972d6118460b005bc0f7bd5a7f8115f3b
Vibhu Sharma <vibhu14sharma@gmail.com>
2023-12-04 17:11:53 +0530

    Function's Question

---
+++ src/Function and methods/Functions.java
@@ -0,0 +1,41 @@
+// https://docs.google.com/document/d/1XB2d4kTzhvmMrvYMgy53AzM4q9iDAxQq7pZ69a2IJjs/edit
+// q. Write a function to calculate the factorial of a number.
+import java.util.*;
+public class Functions {
+    // public static int calculateSum(int a, int b) {
+    //     int sum = a + b;
+    //     return sum;
+    // }
+
+
+    // public static int calculateProduct(int a, int b) {
+    //    return a * b;
+    // }
+
+
+    public static void printFactorial(int n) {
+        //loop
+        if(n < 0) {
+            System.out.println("Invalid Number");
+            return;
+        }
+        int factorial = 1;
+
+
+        for(int i=n; i>=1; i--) {
+            factorial = factorial * i;
+        }
+
+
+        System.out.println(factorial);
+        return;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        printFactorial(n);
+    }
+}
+
